10f1fa32abdd0bba62ea2d8c17a93f3d6177ca9e
Zoggy <zoggy@bat8.org>
2013-03-19 12:10:21 +0100

    update test program; test passed

--- rsstest.ml
+++ rsstest.ml
@@ -31,7 +31,7 @@ let main () =
   if Array.length Sys.argv < 2 then
     fatal (Printf.sprintf "Usage: %s <rss file>" Sys.argv.(0));
   try
-    let channel = Rss.channel_of_file Sys.argv.(1) in
+    let (channel, errors) = Rss.channel_of_file Sys.argv.(1) in
     Rss.print_channel Format.std_formatter channel
   with
     | Sys_error s | Failure s -> fatal s
